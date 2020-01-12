1ebaac2bf7e0f8f4ab2f079d9a61a55607bd3dca
Levi Costa <lewi.santos@outlook.com>
2020-01-12 20:36:41 -0300

    Implantado If de imagens por horário

--- relogio.js
+++ relogio.js
@@ -1,9 +1,30 @@
+//Create on 12/01/2020
+//By Lewis
+//função para importar hora:minutos:segundos do sistema
 function carregar() {
     var horario = window.document.getElementById('horario');
-    var img = window.document.getElementById('foto');
+    var img = window.document.getElementById('fotos');
     var data = new Date();
     var hora = data.getHours();
     var minutos = data.getMinutes();
     var segundos = data.getSeconds();
     horario.innerHTML = `${hora} : ${minutos} : ${segundos}`
+    //condição par exibição das imagens
+    if(hora >= 00 && hora < 06){
+        //madrugada
+        img.src = 'img/madrugada.jpg'
+
+    }else if (hora >= 06 && hora < 12){
+        //manhã
+        img.src ='img/manhã.jpg'
+
+    }else if (hora >= 12 && hora < 18){
+        //Boa tarde
+        img.src = 'img/tarde.jpg'
+
+    } else {
+        //Boa noite
+        img.src = 'img/noite.jpg'
+
+    }
 }
